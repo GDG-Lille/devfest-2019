e6b7431137fc5425a1a523fdd31e74fe4691659f
Angela Ricci <contact@gericci.me>
2018-11-25 16:53:37 +0100

    PWA/FAQ/Code conduite
    - manifest
    - sw
    - faq
    - code de conduite
    - icones

---
+++ sw.js
@@ -0,0 +1,47 @@
+const cacheName = 'devfestlille';
+
+const filesToCache = [
+  '/',
+  '/js/load.js',
+  '/css/main.css',
+  '/css/queries.css',
+  '/css/vars.css',
+  '/css/barlowsemicondensed-bold-webfont.woff',
+  '/css/barlowsemicondensed-italic-webfont.woff',
+  '/css/barlowsemicondensed-regular-webfont.woff',
+  '/css/boogaloo-ragular-webfont.woff',
+  '/css/boogaloo-ragular-webfont.woff2',
+  '/img/logo.svg',
+  '/img/logo-big.svg',
+  '/img/vase.svg',
+  '/img/hifi.svg',
+  '/img/tablet.svg'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
